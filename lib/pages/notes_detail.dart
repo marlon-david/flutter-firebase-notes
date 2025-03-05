f8fca4d0089c6e6dbe9dd9e9a449f103bc8b4b90
Marlon Oliveira <marlon_dg_oliveira@outlook.com>
2025-03-05 09:08:40 -0300

    atualizando pagina de detalhes

--- lib/pages/notes_detail.dart
+++ lib/pages/notes_detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/note.dart';
+import 'notes_form.dart';
 
 class NotesDetail extends StatelessWidget {
   const NotesDetail({super.key, required this.note});
@@ -9,19 +10,39 @@ class NotesDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) => new Scaffold(
       appBar: new AppBar(title: new Text(note.title ?? "Sem título")),
-      body: new Center(
-        child: new Container(
-          width: 250.0,
-          height: 250.0,
-          decoration: new BoxDecoration(
-            color: const Color(0xff7c94b6),
-            borderRadius: new BorderRadius.all(new Radius.circular(75.0)),
-            border: new Border.all(color: Colors.red, width: 4.0),
-          ),
-          child: Text(note.description.toString(),
-          style: const TextStyle(fontFamily: "monospace",
-              fontFamilyFallback: <String>["Courier"]),
-          softWrap: true)
-        ),
-      ));
+      body:Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                '${note.description}',
+                style: const TextStyle(fontSize: 15),
+              )),
+          Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                note.created_at ?? '',
+                style: const TextStyle(fontSize: 12, color: Colors.grey),
+              )),
+          Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                note.updated_at ?? '',
+                style: const TextStyle(fontSize: 12, color: Colors.grey),
+              )),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              new MaterialPageRoute(
+                  builder: (BuildContext context) =>
+                  new NotesForm()));
+        },
+        tooltip: 'Editar nota',
+        child: const Icon(Icons.edit),
+      ),
+  );
 }
